68260dadf8774c09d31c4c9bc7f9e07be7a8319f
latif <latifsha1@outlook.com>
2024-04-27 18:46:33 +0500

    s

---
+++ main.js
@@ -0,0 +1,111 @@
+const one = document.querySelector("#one")
+const one2 = document.querySelector("#one2")
+const two = document.querySelector("#two")
+const two2 = document.querySelector("#two2")
+const three = document.querySelector("#three")
+
+const three2 = document.querySelector("#three2")
+const four = document.querySelector("#four")
+const four2 = document.querySelector("#four2")
+
+const five = document.querySelector("#five")
+const five2 = document.querySelector("#five2")
+const siks = document.querySelector("#siks")
+
+const siks2 = document.querySelector("#siks2")
+const seven = document.querySelector("#seven")
+const seven2 = document.querySelector("#seven2")
+
+const eight = document.querySelector("#eight")
+const eight2 = document.querySelector("#eight2")
+const nine = document.querySelector("#nine")
+
+const nine2 = document.querySelector("#nine2")
+const ten = document.querySelector("#ten")
+const ten2 = document.querySelector("#ten2")
+
+
+one.addEventListener("click" , () =>{
+    console.log("a");
+    one2.style.display = "block"
+})
+one2.addEventListener("click" , () =>{
+    console.log("a");
+    one2.style.display = "none"
+})
+two.addEventListener("click" , () =>{
+    console.log("a");
+    two2.style.display = "block"
+})
+two2.addEventListener("click" , () =>{
+    console.log("a");
+    two2.style.display = "none"
+})
+one2.addEventListener("click" , () =>{
+    console.log("a");
+    one2.style.display = "none"
+})
+three.addEventListener("click" , () =>{
+    console.log("a");
+    three2.style.display = "block"
+})
+three2.addEventListener("click" , () =>{
+    console.log("a");
+    three2.style.display = "none"
+})
+four.addEventListener("click" , () =>{
+    console.log("a");
+    four2.style.display = "block"
+})
+four2.addEventListener("click" , () =>{
+    console.log("a");
+    four2.style.display = "none"
+})
+five.addEventListener("click" , () =>{
+    console.log("a");
+    five2.style.display = "block"
+})
+five2.addEventListener("click" , () =>{
+    console.log("a");
+    five2.style.display = "none"
+})
+siks.addEventListener("click" , () =>{
+    console.log("a");
+    siks2.style.display = "block"
+})
+siks2.addEventListener("click" , () =>{
+    console.log("a");
+    siks2.style.display = "none"
+})
+seven.addEventListener("click" , () =>{
+    console.log("a");
+    seven2.style.display = "block"
+})
+seven2.addEventListener("click" , () =>{
+    console.log("a");
+    seven2.style.display = "none"
+})
+eight.addEventListener("click" , () =>{
+    console.log("a");
+    eight2.style.display = "block"
+})
+eight2.addEventListener("click" , () =>{
+    console.log("a");
+    eight2.style.display = "none"
+})
+nine.addEventListener("click" , () =>{
+    console.log("a");
+    nine2.style.display = "block"
+})
+nine2.addEventListener("click" , () =>{
+    console.log("a");
+    nine2.style.display = "none"
+})
+ten.addEventListener("click" , () =>{
+    console.log("a");
+    ten2.style.display = "block"
+})
+ten2.addEventListener("click" , () =>{
+    console.log("a");
+    ten2.style.display = "none"
+})
